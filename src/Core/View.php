91a25528f87938944839244187882833f1a5fa94
Shivrag Shukla <shivragshukla001@gmail.com>
2025-07-26 02:21:55 +0530

    Added: base file for PDF generator & render raw html method

--- src/Core/View.php
+++ src/Core/View.php
@@ -10,4 +10,18 @@ class View
         require __DIR__ . '/../Views/' . $viewPath . '.php';
     }
 
+    public static function renderRaw(string $viewPath, array $data = []): string
+    {
+        $fullPath = __DIR__ . '/../Views/' . $viewPath . '.php';
+
+        if (!file_exists($fullPath)) {
+            throw new \Exception("View not found: $viewPath");
+        }
+
+        extract($data, EXTR_SKIP);
+        ob_start();
+        include $fullPath;
+        return ob_get_clean();
+    }
+
 }
