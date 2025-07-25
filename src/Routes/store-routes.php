4a074ea4be01ddd70b9e6887a4e9cd03ecbf2bbd
Shivrag Shukla <shivragshukla001@gmail.com>
2025-07-25 18:18:15 +0530

    Updated: Modified store routes to handle create and store actions

--- src/Routes/store-routes.php
+++ src/Routes/store-routes.php
@@ -3,3 +3,5 @@
 use App\Controllers\StoreController;
 
 $router->get('store.php', [StoreController::class, 'index']);
+$router->get('store.php?action=create', [StoreController::class, 'create']);
+$router->post('store.php?action=store', [StoreController::class, 'store']);
